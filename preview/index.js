bc3b855b0e8f9844b1bb618f4eaec94ead2915ac
Jimmy Oliger <hi@jimmyoliger.com>
2015-03-05 15:36:29 +0100

    0.9.0

--- preview/index.js
+++ preview/index.js
@@ -5,7 +5,7 @@ import bootstrap from '../ship/bootstrap';
 var c = {
   appId: manifest.demoKeys.appId,
   orgUrl: manifest.demoKeys.orgUrl,
-  jsUrl: 'https://d3f5pyioow99x0.cloudfront.net/6f8ac94d95c42b46661ec3c10d1258e3e956e40c'
+  jsUrl: 'https://d3f5pyioow99x0.cloudfront.net/6f8ac94d95c42b46661ec3c10d1258e3e956e40c/hull.js'
 };
 
 Hull.init(c, function(hull, me, ship, organization) {
@@ -28,7 +28,7 @@ Hull.init(c, function(hull, me, ship, organization) {
     }
   };
 
-  // TODO use Hull.embed to simulate the real process
+  // TODO use Hull.embed
   bootstrap(document.querySelector(deployment.settings.$selector), deployment);
 });
 
